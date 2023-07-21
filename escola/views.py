3c30520aeeea0395f2579c863758b0e5a1befb3b
af0liveira <af0liveira@icloud.com>
2023-07-21 16:37:18 +0200

    Implement serializer, viewset, and routes for student database (Alunos).
    The serializer has been implemented in `escola/serializers.py` and
    returns the Alunos database in JSON format.
    
    The corresponding viewset has been implemented in `escola/views.py` to
    control the information from the serializer.
    
    Finaly, the default routes have been implemented in `setup/urls.py`.
    
    In principle, these changes should take effect immediately, without the
    need to restart the server or apply migrations.
    
    The URL `127.0.0.1:8000/alunos` should show the Django REST framework
    interface and should allow some tests with the HTTP requests.

--- escola/views.py
+++ escola/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from escola.models import Aluno
+from escola.serializers import AlunoSerializer
+
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+
+from escola.views import AlunosViewSet
+
+# Create a router and register routes
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='Alunos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
