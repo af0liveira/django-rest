166d392af1262a0fa57f8841a08c03a03fd105d0
af0liveira <af0liveira@icloud.com>
2023-07-21 02:12:34 +0200

    Implement models for Aluno.
    This commit implements the model for representing a student (Aluno), as
    well as the model for the respective CRUD operations with Django Admin.
    
    In addition to the implementation, the 'escola' app has been included in
    the `INSTALLED_APPS` list in `setup/settings.py`, followed by the
    criation and application of the migrations
    
    ```sh
    python manage.py makemigrations
    python manage.py migrate
    ```

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Model):
+    """Class to represent a student."""
+
+    nome = models.CharField(max_length=50)
+    rg = models.CharField(max_length=9)
+    cpf = models.CharField(max_length=11)
+    data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome
+    
